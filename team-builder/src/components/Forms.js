bce8c7d7a79ccf3b4f0e8d7b589b855cfdf93bd7
Zolton <gmzesq@gmail.com>
2019-07-15 14:46:34 -0400

    Forms set up and work

--- team-builder/src/components/Forms.js
+++ team-builder/src/components/Forms.js
@@ -1,22 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 function Form() {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
 
-    const [userData, setUserData] = useState({
-        name: "",
-        email: "",
-        role: ""
-    })
+  const changeHandler = event => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value
+    });
+  };
 
-    return (
-        <>
-        <div>
-            Hello from Forms</div>
-            <input />
-            <input />
-            <input />
-        </>
-    )
+  return (
+    <>
+      <div>Hello from Forms</div>
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter username"
+          onChange={changeHandler}
+          value={userData.name}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter email"
+          onChange={changeHandler}
+          value={userData.email}
+        />
+        <input
+          type="text"
+          name="role"
+          placeholder="Enter role"
+          onChange={changeHandler}
+          value={userData.role}
+        />
+      </form>
+    </>
+  );
 }
 
-export default Form
+export default Form;
