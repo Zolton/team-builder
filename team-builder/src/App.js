a858648f96da8c86c9bc5cb6cd0262cb91795064
Zolton <gmzesq@gmail.com>
2019-07-15 16:59:07 -0400

    setMemberToEdit hook working, latching onto only team member clicked

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -3,30 +3,33 @@ import "./App.css";
 import Form from "./components/Forms";
 
 function App() {
-  const [teamData, setTeamData] = useState([{ name: "", email: "", role: "" }]);
+  const [teamData, setTeamData] = useState([]);
   const [memberToEdit, setMemberToEdit] = useState();
 
   return (
     <div className="App">
       Team Builder App
-      <Form setTeamData={setTeamData} setMemberToEdit={setMemberToEdit} teamData={teamData} />
+      <Form
+        setTeamData={setTeamData}
+        setMemberToEdit={setMemberToEdit}
+        teamData={teamData}
+      />
       {console.log("this is team data in App")}
       {console.log({ teamData })}
       <h2> This is the data being passed to App</h2>
-      {teamData.map(name=><div>
-      <h3>{name.name}</h3>
-      <h3>{name.email}</h3>
-      <h3>{name.role}</h3></div>
-      )}
-      {/* <button>Edit Name</button>
-      <h3>{teamData.map(email=>
-        email.email
-        )}</h3>
-      <button>Edit email</button>
-      <h3>{teamData.map(role=>
-        role.role
-        )}</h3>
-      <button>Edit role</button> */}
+      {teamData.map(name => (
+        <div>
+          <h3>{name.name}</h3>
+          <h3>{name.email}</h3>
+          <h3>{name.role}</h3>
+          <button onClick={() => setMemberToEdit(name)}>
+            Edit Team Member name
+          </button>
+          {console.log("This is memberToEdit")}
+          {console.log(memberToEdit)}
+        </div>
+      ))}
+      
     </div>
   );
 }
